a1fdde2fed7532d54996208af3c33b94a42e876f
Stuart Thomas <stuart.thomas@zoopla.co.uk>
2022-07-17 10:55:24 +0100

    MonkeyTests

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Automated-Monkey-Testing-Demo
 A simple automated monkey testing demo using Playwright and gremlins.js written in typescript
+
+## How to run the tests
+Be sure to have installed playwright.  If you havent already, use the following command
+
+npm i playwright
+
+Then from the root folder (where this readme is located) run the below command
+
+npx playwright test --headed
+
+Chromium will open and you will be able to see the gremlins/monkeys at work
---
+++ tests/monkey.spec.ts
@@ -0,0 +1,43 @@
+import { test } from '@playwright/test';
+
+const { describe, beforeEach, expect } = test;
+let errors = 0;
+
+describe.only('Monkey test', () => {
+	beforeEach(async ({ page }) => {
+		page.on('console', (msg) => {
+			if (msg.type() === 'error') {
+				console.log(`🐟`, msg);
+				errors++;
+			}
+		});
+
+		page.on('popup', (newPage) => {
+			newPage.close();
+		});
+
+		await page.addInitScript({
+			path: './node_modules/gremlins.js/dist/gremlins.min.js',
+		});
+
+		await page.goto('https://demo.microfrontends.com/', {
+			waitUntil: 'domcontentloaded',
+		});
+	});
+
+	test('should fail if there are 10 or more errors logged', async ({
+		page,
+	}) => {
+		await page.evaluateHandle(() => {
+			return gremlins
+				.createHorde({
+					randomizer: new gremlins.Chance(1234), 
+					strategies: [gremlins.strategies.allTogether({ nb: 100 })],
+				})
+				.unleash();
+		});
+
+		expect(errors).toBeLessThanOrEqual(9);
+		console.log('errors ' + errors);
+	});
+});
